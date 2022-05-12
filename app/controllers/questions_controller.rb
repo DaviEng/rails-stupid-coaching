aefee4f97664176ede51e2572db01eb7b0d975d3
Davi Rocha <davipradorocha@gmail.com>
2022-05-12 14:05:08 -0300

    Alteração answer controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:question]
-    @response = nil
-    if @answer.downcase == "i am going to work right now!"
+    @question = params[:question]
+    if @question.blank?
+      @response = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
       @response = "Great!"
-    elsif @answer.end_with?("?")
+    elsif @question.ends_with?("?")
       @response = "Silly question, get dressed and go to work!"
     else
       @response = "I don't care, get dressed and go to work!"
